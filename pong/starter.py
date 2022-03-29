82f756168608afde86250644f95eea2d4d4d87a0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-29 17:18:55 +0100

    created ball and paddles

--- pong/starter.py
+++ pong/starter.py
@@ -1,12 +1,38 @@
 
 import turtle
 
+from numpy import pad
+
 wn = turtle.Screen()
 wn.title("PingPong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Adding paddles 
+# paddle A
+paddleA = turtle.Turtle()
+paddleA.speed(0)
+paddleA.shape("square")
+paddleA.color("white")
+paddleA.shapesize(stretch_wid=5 , stretch_len=1)
+paddleA.penup()
+paddleA.goto(-350,0)
+# paddle B
+paddleB = turtle.Turtle()
+paddleB.speed(0)
+paddleB.shape("square")
+paddleB.color("white")
+paddleB.shapesize(stretch_wid=5 , stretch_len=1)
+paddleB.penup()
+paddleB.goto(350,0)
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
 # main game loop
 while True:
   wn.update()
