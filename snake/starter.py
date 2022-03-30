f90277b434b7071af7f4282375433c162ebea520
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-30 09:35:51 +0100

    creating the basic class

---
+++ snake/starter.py
@@ -0,0 +1,26 @@
+
+import math
+import random
+import tkinter as tk
+from tkinter import messagebox
+import pygame 
+
+class cube(object):
+  rows = 0
+  w = 0
+  def __init__(self ,start ,dirnx =1 ,  dirny = 0 , color=(255,0,0)):
+      pass
+  def move(self, drinx , dirny):
+    pass
+  def draw(self,surface , sys = False):
+    pass
+  
+class snake(object):
+  def __init__(self, color , pos):
+      pass
+    
+  def move(self):
+    pass
+  def reset(self,pos):
+    pass
+  def  
