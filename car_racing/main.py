1bf96458c105273f7059b495331794ca99ab5d56
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-03-12 07:48:32 +0100

    adding resources

---
+++ car_racing/main.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+import time
+import math
+from utils import scale_image
+
+GRASS = pygame.image.load("imgs/grass.jpg")
+TRACK = pygame.image.load("imgs/track.png")
+TRACK_BORDER = pygame.image.load("imgs/track-border.png")
+
+FINISH = pygame.image.load("imgs/finish.png")
+FINISH_MASK = pygame.mask.from_surface(FINISH)
+FINISH_POSITION = (130, 250)
+
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
